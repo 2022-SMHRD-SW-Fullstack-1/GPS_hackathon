b1b3c9a999d238616ce93088d997e9ea109ab031
노한서 <112401561+ignaciocha@users.noreply.github.com>
2022-12-12 14:39:23 +0900

    intro activity

--- app/src/main/java/com/example/gps/user/JoinActivity.kt
+++ app/src/main/java/com/example/gps/user/JoinActivity.kt
@@ -4,11 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gps.R
 
-class LoginActivity : AppCompatActivity() {
+class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
-
+        setContentView(R.layout.activity_join)
     }
 }
