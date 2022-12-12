caed23b85693fbbce51b03afe8891cc43b111ab6
노한서 <112401561+ignaciocha@users.noreply.github.com>
2022-12-12 11:48:50 +0900

    init commit

--- app/src/main/java/com/example/gps/user/LoginActivity.kt
+++ app/src/main/java/com/example/gps/user/LoginActivity.kt
@@ -1,12 +1,12 @@
-package com.example.gps.home
+package com.example.gps.user
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gps.R
 
-class HomeActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_login)
     }
 }
