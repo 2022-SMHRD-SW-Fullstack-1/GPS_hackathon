15af52b7ccca9e8746d018dc741838923e46cc99
유성용 <suseong77@naver.com>
2022-12-12 14:45:47 +0900

    first commit

--- app/src/main/java/com/example/gps/home/HomeActivity.kt
+++ app/src/main/java/com/example/gps/home/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.example.gps.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gps.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
