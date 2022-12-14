91b90e942045e6478ad10fce73dee6d525255c34
임다인 <sayou1214@gmail.com>
2022-12-14 14:13:41 +0900

    날씨API

--- app/src/main/java/com/example/gps/MainActivity.kt
+++ app/src/main/java/com/example/gps/MainActivity.kt
@@ -1,23 +1,21 @@
 package com.example.gps
 
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Rect
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Base64
 import android.util.Log
-import android.view.MotionEvent
-import android.view.inputmethod.InputMethodManager
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.gps.fragment.*
-import com.example.gps.location.LocationActivity
+import com.example.gps.weather.WeatherVO
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import java.security.MessageDigest
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,31 +23,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        try {
-            val information =
-                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
-            val signatures = information.signingInfo.apkContentsSigners
-            val md = MessageDigest.getInstance("SHA")
-            for (signature in signatures) {
-                val md: MessageDigest
-                md = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-                var hashcode = String(Base64.encode(md.digest(), 0))
-                Log.d("hashcode", "" + hashcode)
-            }
-        } catch (e: Exception) {
-            Log.d("hashcode", "에러::" + e.toString())
 
-        }
+        //프로젝트 키 해시 가져오는 코드
+//        try {
+//            val information =
+//                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+//            val signatures = information.signingInfo.apkContentsSigners
+//            val md = MessageDigest.getInstance("SHA")
+//            for (signature in signatures) {
+//                val md: MessageDigest
+//                md = MessageDigest.getInstance("SHA")
+//                md.update(signature.toByteArray())
+//                var hashcode = String(Base64.encode(md.digest(), 0))
+//                Log.d("hashcode", "" + hashcode)
+//            }
+//        } catch (e: Exception) {
+//            Log.d("hashcode", "에러::" + e.toString())
+//        }
 
+        val imgWeather = findViewById<ImageView>(R.id.imgWeather)
+        val tvWeather = findViewById<TextView>(R.id.tvWeather)
         val bnv = findViewById<BottomNavigationView>(R.id.bnv)
         val fl = findViewById<FrameLayout>(R.id.fl)
         val imgLogout = findViewById<ImageView>(R.id.imgLogout)
         val tvMap = findViewById<TextView>(R.id.tvMap)
 
+        val weatherList = ArrayList<WeatherVO>()
+
         tvMap.setOnClickListener {
             val intent = Intent(this@MainActivity, MapActivity::class.java)
-//            val intent = Intent(this@MainActivity, LocationActivity::class.java)
             startActivity(intent)
         }
 
@@ -57,6 +59,64 @@ class MainActivity : AppCompatActivity() {
             R.id.fl,
             HomeFragment()
         ).commit()
+
+        //Volley 네트워크 통신 4단계
+        //1. Volley 라이브러리 설정
+        //1-1. 라이브러리 추가, 인터넷 권한, 요청 방식이 http 인지 https인지 !
+        //1-2. http면 manifests에 android:usesCleartextTraffic="true" 추가 하기!
+        //2. RequestQueue 생성
+        val requestQueue = Volley.newRequestQueue(this@MainActivity)
+
+        //3. Request 생성
+        val cityList = ArrayList<String>()
+        cityList.add("Gwangju")
+
+        val requestList = ArrayList<StringRequest>()
+
+        val index = 0
+
+        val url =
+            "https://api.openweathermap.org/data/2.5/weather?q=${cityList[index]}&appid=0191cb6e487f18ad2edd5dcf1dc5e49a&units=metric"
+
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            { response ->
+                Log.d("날씨 $index", response)
+                //weather[0] → main
+                val result = JSONObject(response)
+                val weathers = result.getJSONArray("weather")
+                val weather = weathers[0] as JSONObject //as JSONObject JSONObject로 다운캐스팅한다는 뜻
+                val state = weather.getString("main")
+
+                //main → temp, humidity
+                val main = result.getJSONObject("main")
+                val temp = main.getString("temp")
+
+                Log.d("현재날씨$index", "상태: $state, 온도: $temp")
+                weatherList.add(WeatherVO(cityList[index], state, temp))
+            },
+            {}
+        )
+        requestList.add(request)
+
+
+        for (i in 0 until requestList.size) {
+            requestQueue.add(requestList[i])
+        }
+
+        imgWeather.setOnClickListener {
+            if(weatherList.get(0).state == "Clear"){
+                imgWeather.setImageResource(R.drawable.sunny)
+            }else if(weatherList.get(0).state == "Snow"){
+                imgWeather.setImageResource(R.drawable.snow)
+            }else if (weatherList.get(0).state == "Clouds"){
+                imgWeather.setImageResource(R.drawable.cloudy)
+            }else if(weatherList.get(0).state == "Rain"){
+                imgWeather.setImageResource(R.drawable.rain)
+            }
+            tvWeather.text = weatherList.get(0).temp
+        }
 
         bnv.setOnItemSelectedListener { item ->
             // item -> 내가 선택한 item의 정보
@@ -99,27 +159,6 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-
-    // 텍스트 입력 박스 이외 다른 영역 클릭시 키보드 내리기
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        val focusView = currentFocus
-        if (focusView != null && ev != null) {
-            val rect = Rect()
-            focusView.getGlobalVisibleRect(rect)
-            val x = ev.x.toInt()
-            val y = ev.y.toInt()
-
-            if (!rect.contains(x, y)) {
-                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-                imm?.hideSoftInputFromWindow(focusView.windowToken, 0)
-                focusView.clearFocus()
-            }
-        }
-        return super.dispatchTouchEvent(ev)
-    }
-
-
-
 
 
 }
