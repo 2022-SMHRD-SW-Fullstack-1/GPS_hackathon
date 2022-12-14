e8928c67106e08aa90cb5873a84766396eba0a7b
주상민 <112377313+js4183@users.noreply.github.com>
2022-12-14 14:36:52 +0900

    채팅 구현

--- app/src/main/java/com/example/gps/chat/RegistrationActivity.kt
+++ app/src/main/java/com/example/gps/chat/RegistrationActivity.kt
@@ -8,6 +8,6 @@ import com.example.gps.R
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_registration)
     }
 }
