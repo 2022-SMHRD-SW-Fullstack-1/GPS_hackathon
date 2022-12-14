c217bb6b107beeccc448b4d0183b16c4779a7d74
임다인 <sayou1214@gmail.com>
2022-12-14 15:05:05 +0900

    날씨API 홈에 추가중

--- app/src/main/java/com/example/gps/MainActivity.kt
+++ app/src/main/java/com/example/gps/MainActivity.kt
@@ -41,8 +41,6 @@ class MainActivity : AppCompatActivity() {
 //            Log.d("hashcode", "에러::" + e.toString())
 //        }
 
-        val imgWeather = findViewById<ImageView>(R.id.imgWeather)
-        val tvWeather = findViewById<TextView>(R.id.tvWeather)
         val bnv = findViewById<BottomNavigationView>(R.id.bnv)
         val fl = findViewById<FrameLayout>(R.id.fl)
         val imgLogout = findViewById<ImageView>(R.id.imgLogout)
@@ -60,63 +58,7 @@ class MainActivity : AppCompatActivity() {
             HomeFragment()
         ).commit()
 
-        //Volley 네트워크 통신 4단계
-        //1. Volley 라이브러리 설정
-        //1-1. 라이브러리 추가, 인터넷 권한, 요청 방식이 http 인지 https인지 !
-        //1-2. http면 manifests에 android:usesCleartextTraffic="true" 추가 하기!
-        //2. RequestQueue 생성
-        val requestQueue = Volley.newRequestQueue(this@MainActivity)
 
-        //3. Request 생성
-        val cityList = ArrayList<String>()
-        cityList.add("Gwangju")
-
-        val requestList = ArrayList<StringRequest>()
-
-        val index = 0
-
-        val url =
-            "https://api.openweathermap.org/data/2.5/weather?q=${cityList[index]}&appid=0191cb6e487f18ad2edd5dcf1dc5e49a&units=metric"
-
-        val request = StringRequest(
-            Request.Method.GET,
-            url,
-            { response ->
-                Log.d("날씨 $index", response)
-                //weather[0] → main
-                val result = JSONObject(response)
-                val weathers = result.getJSONArray("weather")
-                val weather = weathers[0] as JSONObject //as JSONObject JSONObject로 다운캐스팅한다는 뜻
-                val state = weather.getString("main")
-
-                //main → temp, humidity
-                val main = result.getJSONObject("main")
-                val temp = main.getString("temp")
-
-                Log.d("현재날씨$index", "상태: $state, 온도: $temp")
-                weatherList.add(WeatherVO(cityList[index], state, temp))
-            },
-            {}
-        )
-        requestList.add(request)
-
-
-        for (i in 0 until requestList.size) {
-            requestQueue.add(requestList[i])
-        }
-
-        imgWeather.setOnClickListener {
-            if(weatherList.get(0).state == "Clear"){
-                imgWeather.setImageResource(R.drawable.sunny)
-            }else if(weatherList.get(0).state == "Snow"){
-                imgWeather.setImageResource(R.drawable.snow)
-            }else if (weatherList.get(0).state == "Clouds"){
-                imgWeather.setImageResource(R.drawable.cloudy)
-            }else if(weatherList.get(0).state == "Rain"){
-                imgWeather.setImageResource(R.drawable.rain)
-            }
-            tvWeather.text = weatherList.get(0).temp
-        }
 
         bnv.setOnItemSelectedListener { item ->
             // item -> 내가 선택한 item의 정보
